fbe18be3b1313375b76369a5d398480b7d8f8d10
maosong327 <47300316+maosong327@users.noreply.github.com>
2024-07-09 16:38:08 +0800

    Update correlation_betwwn_ERVs_and_genes.R

--- scripts/3 correlation_analysis/correlation_betwwn_ERVs_and_genes.R
+++ scripts/3 correlation_analysis/correlation_betwwn_ERVs_and_genes.R
@@ -3,6 +3,7 @@
 # Goldman, M.J., Craft, B., Hastie, M. et al. Visualizing and interpreting
 # cancer genomics data via the Xena platform. Nat Biotechnol (2020).
 # https://doi.org/10.1038/s41587-020-0546-8
+
 #1、input genes expression matrix.
 genes.raw <- read.table("rawData/tcga_RSEM_Hugo_norm_count",header=TRUE,sep="\t")
 genes.raw <- as.data.frame(genes.raw)
@@ -15,6 +16,7 @@ genes.raw.log2 <- as.data.frame(genes.raw.log2)
 genes.raw.log2.t <- t(genes.raw.log2)
 genes.raw.log2.t <- as.data.frame(genes.raw.log2.t)
 genes.raw.log2.t$sample <- rownames(genes.raw.log2.t)
+
 #2、input human endogenous retroviral sequences (HERVs) expression matrix
 erv.raw <- read.table("rawData/TCGA_tissue_samples_ERV_CPM.txt",header=TRUE,sep="\t")
 #include only tumor tissue samples.
@@ -29,6 +31,7 @@ cancer.mean <- cancer.mean[,-1]
 cancer.mean.log2 <- log2(cancer.mean+1)
 cancer.mean.log2$sample <- rownames(cancer.mean.log2)
 cancer.mean.log2 <- as.data.frame(cancer.mean.log2)
+
 #3、merge genes and HERVs expression matrix
 erv.genes <- merge(genes.raw.log2.t, cancer.mean.log2,by="sample")
 rownames(erv.genes) <- erv.genes$sample
@@ -44,6 +47,7 @@ chosen.gene$V1 <- gsub(";","",chosen.gene$V1)
 chosen.erv.genes <- erv.genes[,colnames(erv.genes) %in% c(chosen.gene$V1,colnames(erv.raw))]
 chosen.erv.genes <- as.data.frame(chosen.erv.genes)
 chosen.erv.genes$sample <- rownames(chosen.erv.genes)
+
 #4、correlation analysis and output result
 library(Hmisc)
 cor.res <- rcorr(as.matrix(chosen.erv.genes,method="pearson"))
@@ -57,6 +61,7 @@ write.table(as.data.frame(res.p),file=paste(chosen,"P_value.txt",sep="_"),sep="\
 # remove genes and genes, HERVs and HERVs
 res.r <- res.r.raw[rownames(res.r.raw) %in% colnames(erv.raw),colnames(res.p.raw) %in% chosen.gene$V1]
 write.table(as.data.frame(res.r),file=paste(chosen,"r_value.txt",sep="_"),sep="\t")
+
 # 5、output raw data, i.e. expression value for correlation analysis
 anno <- erv.raw[,c(1,2)]
 anno$sample <- substr(anno$submitter_id,1,15)
